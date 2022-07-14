8df8fb8302ec83fb52e2912e680722d75849606a
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-07-14 00:43:17 -0300

    Add files via upload

---
+++ parametros e html express js/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/html/index.html");
+});
+
+
+app.get("/sobre", function(req, res){
+    res.sendFile(__dirname + "/html/sobre.html");
+});
+
+
+app.get("/blog", function(req, res){
+    res.sendFile(__dirname + "/html/blog.html");
+});
+
+app.get("/ola/:cargo/:nome/:cor", function(req, res){
+    res.send("<h1>Olá " + req.params.nome + "!</h1>" + "<h2>Seu cargo é: " + req.params.cargo + ".</h2>" + "<h3>Sua cor favorita é " + req.params.cor + ".</h3>");
+});
+
+
+app.listen(8081, function(){
+    console.log("Servidor rodando na url http://localhost:8081/");
+});

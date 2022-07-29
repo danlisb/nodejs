342ac2339f2fc5d5353b1f0dabbb0cb998d7a27d
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-07-29 18:59:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'red t-shirt',
+        size: 'large'
+    })
+});
+
+
+app.post ('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo){
+        res.status(418).send({ message: 'We need a logo!' });
+    }
+
+    res.send({
+        tshirt: `shirt with your ${logo} and ID of ${id}!`,
+    });
+});
+
+
+app. listen(
+    PORT,
+    () => console.log(`running on http://localhost:${PORT}`)
+)

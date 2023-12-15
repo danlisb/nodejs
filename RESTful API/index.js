eee4f6fc3272fe6618e27a8d68b3dfb862fd4aa3
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2023-12-14 21:16:57 -0300

    Add files via upload

--- RESTful API/index.js
+++ RESTful API/index.js
@@ -4,29 +4,67 @@ const PORT = 8080;
 
 app.use(express.json());
 
-app.get('/tshirt', (req, res) => {
-    res.status(200).send({
-        tshirt: 'red t-shirt',
-        size: 'large'
-    })
+// Dados de exemplo para simular um banco de dados temporário
+const camisetas = {
+  1: {
+    camiseta: 'Camiseta vermelha',
+    tamanho: 'G',
+    marca: 'Adidas'
+  },
+  2: {
+    camiseta: 'Camiseta preta',
+    tamanho: 'P',
+    marca: 'Nike'
+  }
+}
+
+// Rota para obter todas as camisetas
+app.get('/camisetas', (req, res) => {
+  res.status(200).json(camisetas);
 });
 
+// Rota para criar uma nova camiseta (POST)
+app.post('/camisetas', (req, res) => {
+  const { camiseta, tamanho, marca } = req.body;
 
-app.post ('/tshirt/:id', (req, res) => {
-    const { id } = req.params;
-    const { logo } = req.body;
+  if (!camiseta || !tamanho || !marca) {
+    return res.status(400).json({ error: 'Preencha todos os campos: camiseta, tamanho, marca' });
+  }
 
-    if (!logo){
-        res.status(418).send({ message: 'We need a logo!' });
-    }
+  // Simulando a criação de uma nova camiseta
+  const novaCamisetaId = Object.keys(camisetas).length + 1;
+  camisetas[novaCamisetaId] = { camiseta, tamanho, marca };
 
-    res.send({
-        tshirt: `shirt with your ${logo} and ID of ${id}!`,
-    });
+  return res.status(201).json(camisetas[novaCamisetaId]);
 });
 
+// Rota para obter uma camiseta específica
+app.get('/camisetas/:id', (req, res) => {
+  const camisetaId = req.params.id;
+  const camiseta = camisetas[camisetaId];
 
-app. listen(
-    PORT,
-    () => console.log(`running on http://localhost:${PORT}`)
-)
+  if (!camiseta) {
+    return res.status(404).json({ error: 'Camiseta não encontrada' });
+  }
+
+  return res.status(200).json(camiseta);
+});
+
+// Rota para excluir uma camiseta (DELETE)
+app.delete('/camisetas/:id', (req, res) => {
+  const camisetaId = req.params.id;
+  const camiseta = camisetas[camisetaId];
+
+  if (!camiseta) {
+    return res.status(404).json({ error: 'Camiseta não encontrada' });
+  }
+
+  // Simulando a exclusão de uma camiseta
+  delete camisetas[camisetaId];
+
+  return res.status(200).json(camiseta);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
